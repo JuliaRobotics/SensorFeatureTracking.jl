732d9eb4c56ca029926ae528730fb09c4a6ebe6c
Johannes Terblanche <terblanche.j@gmail.com>
2017-12-07 16:10:01 +0200

    update livetracking

--- examples/livetracking.jl
+++ examples/livetracking.jl
@@ -10,43 +10,7 @@ datadir = "../Data"
 
 ## #################################################################################################
 # functions
-function producer(c::Channel)
 
-    N = 100
-    ##
-    set_io_method(Video4Linux.IO_METHOD_READ)
-
-    ## open device
-    fid = open_device("/dev/video0")
-
-    ## init_device(fd, force_format);
-    init_device(fid)
-
-    ## start_capturing(fd);
-    start_capturing(fid)
-
-
-    imy = zeros(UInt8,480,640)
-    for i = 1:N
-        mainloop( fid, 1 )
-        ## copy_buffer_bytes, copy the image buffer bytes to uint8 vector, the lenght will depend on the pixel format
-        imbuff = copy_buffer_bytes(640*480*2)
-        imy[:,:] =  reshape(imbuff[2:2:end],(640,480))'
-        put!(c, imy)
-    end
-
-    ## stop_capturing(fd);
-    stop_capturing(fid)
-
-    ## uninit_device();
-    uninit_device(fid)
-
-    ## close device
-    close_device(fid)
-end
-
-
-#
 function tracklive(im1, tracker, vidchan)
 
     im1[:] = Gray{Float32}.(take!(vidchan)/255)
@@ -66,18 +30,21 @@ end
 
 ## ##################################################################################################
 #run klt on live feed
-vidchan = Channel(producer)
+# producer(c::Channel) = vidproducer(c)
+yonly = Y422(640,480)
+# create a videoproducer type channel (will only run 100 frames)
+vidchan = Channel((c::Channel) -> videoproducer(c, yonly, N = 100))
 
 ##
-
+#capture one frame to create im1 and canvas needed to diplay
 im1 = Gray{Float32}.(take!(vidchan)/255)
 canvas = imshow(im1)
+
 
 ##
 im1 = Gray{Float32}.(take!(vidchan)/255)
 (ro,co) = size(im1)
-kpoints = getApproxBestShiTomasi(im1,nfeatures=50, stepguess=0.95)
-
+kpoints = getApproxBestShiTomasi(im1,nfeatures=20, stepguess=0.95)
 
 
 tracker  = KLTTracker(im1, 20, Float32(0.05), kpoints )
@@ -85,12 +52,7 @@ tracker  = KLTTracker(im1, 20, Float32(0.05), kpoints )
 foreach(ft->drawfeatureX!(im1, CartesianIndex(round.(Int,ft.affinity.v[1:2])...), length=3), tracker.features)
 im1
 
-# wil break when channel is closed
+# run untul channel is closed
 while isopen(vidchan)
     @time tracklive(im1, tracker, vidchan)
-
 end
-
-
-
-##
